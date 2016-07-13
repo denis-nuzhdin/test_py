4f9b029cbe8a095a3758980645becf67896572d3
denis-nuzhdin <kashmir@list.ru>
2016-07-13 22:59:32 +0400

    03

--- add_group.py
+++ add_group.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -16,32 +15,54 @@ class add_group(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_add_group(self):
-        success = True
         wd = self.wd
-        wd.get("http://macbook-pro-denis.local/addressbook/index.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        self.open_site(wd)
+        self.login(wd, username="admin", password="secret")
+        self.fill_group(wd, gr_name="test1", gr_header="tt1", gr_footer="tt2")
+        self.submit(wd)
+        self.returne_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def returne_group_page(self, wd):
+        # return group page
+        wd.find_element_by_link_text("group page").click()
+
+    def submit(self, wd):
+        # submit
+        wd.find_element_by_name("submit").click()
+
+    def fill_group(self, wd, gr_name, gr_header, gr_footer):
+        # fill group
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test1")
+        wd.find_element_by_name("group_name").send_keys("%s" % gr_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("tt1")
+        wd.find_element_by_name("group_header").send_keys("%s" % gr_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("tt2")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
+        wd.find_element_by_name("group_footer").send_keys("%s" % gr_footer)
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("%s" % username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_site(self, wd):
+        # open site
+        wd.get("http://macbook-pro-denis.local/addressbook/index.php")
+
     def tearDown(self):
         self.wd.quit()
 
