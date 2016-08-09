03905eb9e1e79c209f11765aaac29d61bb4ca1b2
denis-nuzhdin <kashmir@list.ru>
2016-08-09 22:37:55 +0400

    добавлены проверки созданий и редактирования групп

--- model/group.py
+++ model/group.py
@@ -1,3 +1,5 @@
+from sys import  maxsize
+
 class Group:
 
     def __init__(self, gr_name=None, gr_header=None, gr_footer=None, id = None):
@@ -5,3 +7,15 @@ class Group:
         self.gr_header = gr_header
         self.gr_footer = gr_footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id,self.gr_name)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.gr_name == other.gr_name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,9 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group (gr_name="testttt1", gr_header="tt1", gr_footer="tt2"))
+    group = Group (gr_name="testttt1", gr_header="tt1", gr_footer="tt2")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len (old_groups) + 1 == len(new_groups)
+    old_groups.append (group)
+    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
